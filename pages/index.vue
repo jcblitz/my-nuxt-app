27db00e7496b60a0cc25a39b23423e8f399cdd85
Jared Blitzstein <mailing-list@blitzstein.net>
2022-02-27 16:17:00 -0500

    Update index.vue
    Created a data object for the cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,38 @@
 <template>
+
+<div>
+
+  <section class="hero">
+    <div class="hero-body">
+      <div class="columns">
+        <div class="column">
+          <p class="title">
+            My Collections
+          </p>
+          <p class="subtitle">
+            A list of my stuff
+          </p>
+        </div>
+        <div class="column level-item has-text-right">
+          <b-button type="is-primary is-light" @click="clickMe">New Collection</b-button>
+        </div>
+      </div>  
+    </div>
+  </section>
+
+<section class="section">
+    <div class="columns is-mobile">
+      <card v-for="collection in collections" :key="collection.name" :title="collection.name" icon="github">
+        Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
+      </card>
+    </div>
+</section>  
+
   <section class="section">
     <div class="columns is-mobile">
-      <card title="Free" icon="github">
+      
+
+      <card title="Guitars" icon="github">
         Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
       </card>
 
@@ -19,6 +50,7 @@
       </card>
     </div>
   </section>
+  </div>
 </template>
 
 <script>
@@ -26,8 +58,39 @@ import Card from '~/components/Card'
 
 export default {
   name: 'IndexPage',
+
   components: {
-    Card,
-  },
+    Card
+  }, data() {
+    return {
+      collections: [
+        {
+          title: "Guitars",
+          icon: null,
+          name: "Guitars",
+          shortDescription: "Nothing fancy, but I like them"
+        }, {
+          title: "Funko Pops",
+          icon: null,
+          name: "Funko",
+          shortDescription: "My bobble heads, mostly Funko"
+        },{
+          title: "Halloween Skeletons",
+          icon: null,
+          name: "Halloween Skeletons",
+          shortDescription: "Nothing fancy, but I like them"
+        }, {
+          title: "Workshop Tools",
+          icon: null,
+          name: "Tools",
+          shortDescription: "My bobble heads, mostly Funko"
+        }
+      ]
+    }
+  }, methods: {
+    clickMe: () => {
+      // no op
+    }
+  }
 }
 </script>
