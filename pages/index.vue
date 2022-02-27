f263efb46ca3623a97f9a1ea18e0f015e8a28af5
Jared Blitzstein <mailing-list@blitzstein.net>
2022-02-27 16:22:03 -0500

    Added updated prettier and ran it. Output committed
    Was getting build failures so took some manual steps to do this

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,54 @@
 <template>
-
-<div>
-
-  <section class="hero">
-    <div class="hero-body">
-      <div class="columns">
-        <div class="column">
-          <p class="title">
-            My Collections
-          </p>
-          <p class="subtitle">
-            A list of my stuff
-          </p>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="columns">
+          <div class="column">
+            <p class="title">My Collections</p>
+            <p class="subtitle">A list of my stuff</p>
+          </div>
+          <div class="column level-item has-text-right">
+            <b-button type="is-primary is-light" @click="clickMe"
+              >New Collection</b-button
+            >
+          </div>
         </div>
-        <div class="column level-item has-text-right">
-          <b-button type="is-primary is-light" @click="clickMe">New Collection</b-button>
-        </div>
-      </div>  
-    </div>
-  </section>
+      </div>
+    </section>
 
-<section class="section">
-    <div class="columns is-mobile">
-      <card v-for="collection in collections" :key="collection.name" :title="collection.name" icon="github">
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
-      </card>
-    </div>
-</section>  
+    <section class="section">
+      <div class="columns is-mobile">
+        <card
+          v-for="collection in collections"
+          :key="collection.name"
+          :title="collection.name"
+          icon="github"
+        >
+          Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
+        </card>
+      </div>
+    </section>
 
-  <section class="section">
-    <div class="columns is-mobile">
-      
+    <section class="section">
+      <div class="columns is-mobile">
+        <card title="Guitars" icon="github">
+          Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
+        </card>
 
-      <card title="Guitars" icon="github">
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
-      </card>
+        <card title="Responsive" icon="cellphone-link">
+          <b class="has-text-grey"> Every </b> component is responsive
+        </card>
 
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey"> Every </b> component is responsive
-      </card>
+        <card title="Modern" icon="alert-decagram">
+          Built with <a href="https://vuejs.org/"> Vue.js </a> and
+          <a href="http://bulma.io/"> Bulma </a>
+        </card>
 
-      <card title="Modern" icon="alert-decagram">
-        Built with <a href="https://vuejs.org/"> Vue.js </a> and
-        <a href="http://bulma.io/"> Bulma </a>
-      </card>
-
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
-    </div>
-  </section>
+        <card title="Lightweight" icon="arrange-bring-to-front">
+          No other internal dependency
+        </card>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -60,37 +59,42 @@ export default {
   name: 'IndexPage',
 
   components: {
-    Card
-  }, data() {
+    Card,
+  },
+  data() {
     return {
       collections: [
         {
-          title: "Guitars",
+          title: 'Guitars',
           icon: null,
-          name: "Guitars",
-          shortDescription: "Nothing fancy, but I like them"
-        }, {
-          title: "Funko Pops",
+          name: 'Guitars',
+          shortDescription: 'Nothing fancy, but I like them',
+        },
+        {
+          title: 'Funko Pops',
           icon: null,
-          name: "Funko",
-          shortDescription: "My bobble heads, mostly Funko"
-        },{
-          title: "Halloween Skeletons",
+          name: 'Funko',
+          shortDescription: 'My bobble heads, mostly Funko',
+        },
+        {
+          title: 'Halloween Skeletons',
           icon: null,
-          name: "Halloween Skeletons",
-          shortDescription: "Nothing fancy, but I like them"
-        }, {
-          title: "Workshop Tools",
+          name: 'Halloween Skeletons',
+          shortDescription: 'Nothing fancy, but I like them',
+        },
+        {
+          title: 'Workshop Tools',
           icon: null,
-          name: "Tools",
-          shortDescription: "My bobble heads, mostly Funko"
-        }
-      ]
+          name: 'Tools',
+          shortDescription: 'My bobble heads, mostly Funko',
+        },
+      ],
     }
-  }, methods: {
+  },
+  methods: {
     clickMe: () => {
       // no op
-    }
-  }
+    },
+  },
 }
 </script>
